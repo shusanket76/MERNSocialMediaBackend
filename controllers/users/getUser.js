dc0701254f3ae727d7818957f794faf70f605d8d
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-07-31 09:49:32 -0400

    user as an array

--- controllers/users/getUser.js
+++ controllers/users/getUser.js
@@ -2,12 +2,11 @@ const User = require("../../models/user");
 const asyncHandler = require("express-async-handler");
 
 const getUsers = asyncHandler(async (req, res) => {
-  
   const user = await User.find().select("-password").lean();
   if (!user.length) {
     return res.json("NO DATA FOUND");
   }
-  return res.json({ message: user });
+  return res.json(user);
 });
 
 module.exports = getUsers;
